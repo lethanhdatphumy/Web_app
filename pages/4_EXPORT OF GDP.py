0b07791af796f00835aec2339d345e8cfc397fb3
Đạtdy Dảk <90812330+lethanhdatphumy@users.noreply.github.com>
2023-07-01 11:18:54 +0700

    Update 4_EXPORT OF GDP.py

--- pages/4_EXPORT OF GDP.py
+++ pages/4_EXPORT OF GDP.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 
-df = pd.read_csv("GOD'sDATA.csv")
+df = pd.read_csv("https://github.com/lethanhdatphumy/Data-Analysis-/blob/ed49225f84d63a1424220cb95f01dea4448166d2/GOD'sDATA.csv")
 df.columns=df.columns.str.strip()
 
 country = st.sidebar.selectbox('Select a Country', df['Country'].unique())
