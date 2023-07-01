2dd7d2f545edf44e3662bd47e82e73d4900ebf6a
Đạtdy Dảk <90812330+lethanhdatphumy@users.noreply.github.com>
2023-07-01 11:37:02 +0700

    Update 6_LIFE EXPECTATION.py

--- pages/6_LIFE EXPECTATION.py
+++ pages/6_LIFE EXPECTATION.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
-df = pd.read_csv("https://github.com/lethanhdatphumy/Data-Analysis-/blob/ed49225f84d63a1424220cb95f01dea4448166d2/GOD'sDATA.csv")
+url = "https://raw.githubusercontent.com/lethanhdatphumy/Data-Analysis-/ed49225f84d63a1424220cb95f01dea4448166d2/GOD'sDATA.csv"
+
+df = pd.read_csv(url)
 df.columns = df.columns.str.strip()
 
 st.sidebar.header('User Input Parameters')
