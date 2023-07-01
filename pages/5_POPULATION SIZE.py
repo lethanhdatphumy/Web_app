e214599294b93a38e17f4b9afda624a443d12adb
Đạtdy Dảk <90812330+lethanhdatphumy@users.noreply.github.com>
2023-07-01 11:36:50 +0700

    Update 5_POPULATION SIZE.py

--- pages/5_POPULATION SIZE.py
+++ pages/5_POPULATION SIZE.py
@@ -4,9 +4,10 @@ import streamlit as st
 import seaborn as sns
 
 
-data = pd.read_csv("https://github.com/lethanhdatphumy/Data-Analysis-/blob/ed49225f84d63a1424220cb95f01dea4448166d2/GOD'sDATA.csv")
-data.columns= data.columns.str.strip()
+url = "https://raw.githubusercontent.com/lethanhdatphumy/Data-Analysis-/ed49225f84d63a1424220cb95f01dea4448166d2/GOD'sDATA.csv"
 
+df = pd.read_csv(url)
+df.columns = df.columns.str.strip()
 
 year = st.sidebar.slider('Select a Year Range', 1990, 2020, (1990, 2020))
 
