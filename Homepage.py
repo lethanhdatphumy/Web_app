c191f33c228b06c7fd7ce234409767006c7f3f6b
Đạtdy Dảk <90812330+lethanhdatphumy@users.noreply.github.com>
2023-07-04 06:45:10 +0700

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -44,7 +44,7 @@ st.header("Data Overview")
 
 data = pd.read_csv("GOD'sDATA.csv")
 data.columns = data.columns.str.strip()
-data["Year"] = pd.to_numeric(data["Year"])
+
 data["Year"] = data["Year"].astype(str)
 st.write(data)
 
