ee75252b36cfdefd6aa33abf2d9fd43c025d202e
Đạtdy Dảk <90812330+lethanhdatphumy@users.noreply.github.com>
2023-07-05 19:33:05 +0700

    Update 7_RATING ECONOMY.py

--- pages/7_RATING ECONOMY.py
+++ pages/7_RATING ECONOMY.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
 
 df = pd.read_csv("GOD'sDATA.csv")
 df.columns = df.columns.str.strip()
@@ -17,17 +16,17 @@ selected_countries = st.sidebar.multiselect('Country', default_countries, defaul
 selected_data = df[df['Country'].isin(selected_countries)]
 rating_counts = selected_data.groupby(['Rating_economy', 'Country']).size().unstack(fill_value=0)
 
-fig, ax = plt.subplots(figsize=(20, 5))
-sns.barplot(data=rating_counts, x=rating_counts.index, y=rating_counts.columns, stacked=True, ax=ax)
+plt.figure(figsize=(20, 5))
+sns.barplot(data=rating_counts, x=rating_counts.index, y=rating_counts.columns, stacked=True)
 
-ax.set_title("RATING ECONOMY OF SOUTHEAST ASIAN COUNTRIES \nData source: World Bank", fontweight='bold', loc='center', color='#FF4500', fontsize=18)
-ax.set_xlabel("Rating economy", fontweight='bold', loc='center', fontsize=18, color='#00AA00')
-ax.set_ylabel("Count", fontweight='bold', fontsize=14, color='#00AA00')
-ax.tick_params(axis='x', labelsize=12)
-ax.tick_params(axis='y', labelsize=12)
+plt.title("RATING ECONOMY OF SOUTHEAST ASIAN COUNTRIES \nData source: World Bank", fontweight='bold', loc='center', color='#FF4500', fontsize=18)
+plt.xlabel("Rating economy", fontweight='bold', loc='center', fontsize=18, color='#00AA00')
+plt.ylabel("Count", fontweight='bold', fontsize=14, color='#00AA00')
+plt.tick_params(axis='x', labelsize=12)
+plt.tick_params(axis='y', labelsize=12)
 sns.set_style('whitegrid')
-ax.set_xticklabels(ax.get_xticklabels(), rotation=15)
+plt.xticks(rotation=15)
 
-ax.legend(title='Country', loc='best', bbox_to_anchor=(0.5, -0.4), ncol=5)
+plt.legend(title='Country', loc='best', bbox_to_anchor=(0.5, -0.4), ncol=5)
 plt.tight_layout()
-st.pyplot(fig)
+st.pyplot(plt)
