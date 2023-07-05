1cecb8ed69bb217fc5901fe2f375d850837869b9
Đạtdy Dảk <90812330+lethanhdatphumy@users.noreply.github.com>
2023-07-05 20:04:19 +0700

    Update 7_RATING ECONOMY.py

--- pages/7_RATING ECONOMY.py
+++ pages/7_RATING ECONOMY.py
@@ -1,48 +1,45 @@
+# Importing necessary libraries
 import streamlit as st
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-df = pd.read_csv(r'path/to/your/file.csv')
+# Read the data
+df = pd.read_csv(r'C:\Users\Dung\Desktop\DataPy.csv')
 
+# Create a sidebar for user inputs
+st.sidebar.header('User Input Parameters')
+rating_selection = st.sidebar.selectbox('Choose Rating Economy Category', ['low income', 'lower middle income', 'upper middle income', 'high income'])
+
+# Filter the data based on the user inputs
+df = df[df['Rating_economy'] == rating_selection]
+
+# Preprocess and visualize data
+Rating_counts = df.groupby(['Rating_economy', 'Country']).size().unstack(fill_value=0)
 rating_counts = df.groupby('Rating_economy').size().reset_index(name='count')
 rating_counts = rating_counts.sort_values('count')
 
 df['Rating_economy'] = pd.Categorical(df['Rating_economy'], categories=rating_counts['Rating_economy'])
 
-Rating_counts = df.groupby(['Rating_economy', 'Country']).size().unstack(fill_value=0)
-Rating_counts = Rating_counts.reindex(['low income', 'lower middle income', 'upper middle income', 'high income'])
+Rating_counts = Rating_counts.reindex([rating_selection])
 
-fig, ax = plt.subplots(figsize=(20, 5))
-Rating_counts.plot(kind='bar', stacked=True, ax=ax)
+# This line will make sure that the app doesn't rerun from the top every time the user interacts with a widget
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
-ax.set_title("RATING ECONOMY OF SOUTHEAST ASIAN COUNTRIES \nData source: World Bank", fontweight='bold', loc='center', color='#FF4500', fontsize=18)
-ax.set_xlabel("Rating economy", fontweight='bold', loc='center', fontsize=14, color='#00AA00')
-ax.set_ylabel("Count", fontweight='bold', fontsize=14, color='#00AA00')
-ax.tick_params(axis='x', labelsize=12)
-ax.tick_params(axis='y', labelsize=12)
+plt.figure(figsize=(20,5))
+Rating_counts.plot(kind='bar', stacked=True)
+
+plt.title(f"RATING ECONOMY OF SOUTHEAST ASIAN COUNTRIES - {rating_selection.upper()} \nData source: World Bank", fontweight='bold', loc='center',color='#FF4500',fontsize=18)
+plt.xlabel("Rating economy", fontweight='bold',loc='center', fontsize=14, color='#00AA00')
+plt.ylabel("Count", fontweight='bold',fontsize=14, color='#00AA00')
+plt.tick_params(axis='x', labelsize=12)
+plt.tick_params(axis='y', labelsize=12)
 sns.set_style('whitegrid')
-ax.set_xticklabels(ax.get_xticklabels(), rotation=15)
+plt.xticks(rotation=15)
+legend = plt.legend(title='Country', loc='lower center', bbox_to_anchor=(0.5, -0.4), ncol=5)
 
-legend = ax.legend(title='Country', loc='lower center', bbox_to_anchor=(0.5, -0.4), ncol=5)
 for text in legend.get_texts():
     text.set_fontweight('semibold')
     text.set_fontsize(12)
-
 plt.tight_layout()
-
-# Streamlit code
-st.title("RATING ECONOMY OF SOUTHEAST ASIAN COUNTRIES")
-st.pyplot(fig)
-
-# Interactive elements
-selected_countries = st.multiselect('Select Countries', df['Country'].unique())
-
-selected_data = df[df['Country'].isin(selected_countries)]
-selected_rating_counts = selected_data.groupby(['Rating_economy', 'Country']).size().unstack(fill_value=0)
-
-st.subheader("Selected Data")
-st.dataframe(selected_data)
-
-st.subheader("Rating Economy Counts for Selected Countries")
-st.dataframe(selected_rating_counts)
+st.pyplot()
