57e26f54cc305d041c26d5db25a3d143e0da1deb
Đạtdy Dảk <90812330+lethanhdatphumy@users.noreply.github.com>
2023-07-05 20:04:50 +0700

    Update 7_RATING ECONOMY.py

--- pages/7_RATING ECONOMY.py
+++ pages/7_RATING ECONOMY.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Read the data
-df = pd.read_csv(r'C:\Users\Dung\Desktop\DataPy.csv')
+df = pd.read_csv(r"GOD'sDATA.csv")
 
 # Create a sidebar for user inputs
 st.sidebar.header('User Input Parameters')
