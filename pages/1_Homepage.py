7b294246b3e0ce76ab52a5acca087a7d0fe67161
Vũ Thanh Lâm <thanhlam03082004@gmail.com>
2023-07-08 13:10:16 +0700

    theo y tuan ko muon hinh

--- pages/1_Homepage.py
+++ pages/1_Homepage.py
@@ -12,9 +12,7 @@ page_bg_img = '''
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
 [data-testid="stAppViewContainer"]  {
-background-image: url("https://images.unsplash.com/photo-1446776653964-20c1d3a81b06?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=871&q=80");
-background-size:cover;
-background-repeat: no-repeat;
+background-color: rgb(6,71,17);
 }
 [data-testid="stHeader"]{
     background-color : rgba(0,0,0,0)
