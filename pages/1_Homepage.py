25c4b13cc1c5ba32aa76861ed1d0f62e895dc365
Đạtdy Dảk <90812330+lethanhdatphumy@users.noreply.github.com>
2023-07-08 20:48:19 +0700

    Update 1_Homepage.py

--- pages/1_Homepage.py
+++ pages/1_Homepage.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
-from pandas import options
 
 st.set_page_config(
     page_title="",
@@ -10,45 +9,37 @@ st.set_page_config(
 
 page_bg_img = '''
 <style>
-[data-testid="stAppViewContainer"]  {
-background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
-background-size:cover;
-background-repeat: no-repeat;
+[data-testid="stAppViewContainer"] {
+    background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
+    background-size: cover;
+    background-repeat: no-repeat;
 }
-[data-testid="stHeader"]{
-    background-color : rgba(0,0,0,0)
-    
+[data-testid="stHeader"] {
+    background-color: rgba(0, 0, 0, 0);
 }
-[data-testid="stSidebar"]{
-background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
-background-size:cover;
-background-repeat: no-repeat;
+[data-testid="stSidebar"] {
+    background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
+    background-size: cover;
+    background-repeat: no-repeat;
 }
-[data-testid="stImage"] > img{
-border-radius:50%;
-
+[data-testid="stImage"] > img {
+    border-radius: 50%;
 }
-[data-testid="stImage"]{
+[data-testid="stImage"] {
 }
 </style>
 '''
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
-
-
-
 data = pd.read_csv("GOD'sDATA.csv")
 data.columns = data.columns.str.strip()
 data["Year"] = data["Year"].astype(str)
 
-
 st.title("Welcome to data visualization")
-st.header("Our dataset: ")
+st.header("Our dataset:")
 
 st.write(data)
-
-
 
 st.sidebar.markdown("## Sidebar")
 st.sidebar.info("Select pages above <3")
