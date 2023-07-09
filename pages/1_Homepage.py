ec762fa616ad435db6ad888dd497d61b2fa0bf58
Đạtdy Dảk <90812330+lethanhdatphumy@users.noreply.github.com>
2023-07-09 15:32:28 +0700

    Update 1_Homepage.py

--- pages/1_Homepage.py
+++ pages/1_Homepage.py
@@ -7,33 +7,7 @@ st.set_page_config(
     page_icon=":wave:",
 )
 
-page_bg_img = '''
-<style>
-[data-testid="stAppViewContainer"]  {
-background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
-background-size:cover;
-background-repeat: no-repeat;
-}
-[data-testid="stHeader"]{
-    background-color : rgba(0,0,0,0)
-    
-}
-[data-testid="stSidebar"]{
-background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
-background-size:cover;
-background-repeat: no-repeat;
-}
-[data-testid="stImage"] > img{
-border-radius:50%;
-width:150px;
 
-}
-[data-testid="stImage"]{
-}
-</style>
-'''
-
-st.markdown(page_bg_img, unsafe_allow_html=True)
 
 data = pd.read_csv("GOD'sDATA.csv")
 
