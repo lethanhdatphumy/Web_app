bd44f692471261f9327876860ff3b5de32872d00
Đạtdy Dảk <90812330+lethanhdatphumy@users.noreply.github.com>
2023-07-09 15:30:43 +0700

    Update 1_Homepage.py

--- pages/1_Homepage.py
+++ pages/1_Homepage.py
@@ -1,33 +1,38 @@
 import streamlit as st
 import pandas as pd
-
-
+from PIL import Image
+import base64
 st.set_page_config(
     page_title="",
     page_icon=":wave:",
 )
+
 page_bg_img = '''
 <style>
-#MainMenu {visibility: hidden;}
-footer {visibility: hidden;}
 [data-testid="stAppViewContainer"]  {
-background-color: rgb(6,71,17);
+background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
+background-size:cover;
+background-repeat: no-repeat;
 }
 [data-testid="stHeader"]{
     background-color : rgba(0,0,0,0)
     
 }
 [data-testid="stSidebar"]{
-background-image: url("https://images.unsplash.com/photo-1483401757487-2ced3fa77952?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=873&q=80");
+background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
 background-size:cover;
 background-repeat: no-repeat;
 }
 [data-testid="stImage"] > img{
 border-radius:50%;
+width:150px;
 
+}
+[data-testid="stImage"]{
 }
 </style>
 '''
+sidebar_image=Image.open(r"C:\Users\HP\Downloads\lmao3.jpg")
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
 data = pd.read_csv("GOD'sDATA.csv")
@@ -35,9 +40,9 @@ data = pd.read_csv("GOD'sDATA.csv")
 
 st.title("Welcome to My" )
 st.header("Data Overview")
-
+data["Year"] = data["Year"].astype(str)
 st.write(data)
 
-
-
-
+st.sidebar.image(sidebar_image,use_column_width=True)
+st.sidebar.markdown("## Sidebar")
+st.sidebar.info("Select pages above <3")
